d8b8192fb22330afd90a0346f175c944d7bf2415
jona4815 <99785626+Raque48@users.noreply.github.com>
2023-04-12 12:39:14 -0600

    Update StockEntity.java

--- src/main/java/com/icodeap/ecommerce/infrastructure/entity/StockEntity.java
+++ src/main/java/com/icodeap/ecommerce/infrastructure/entity/StockEntity.java
@@ -10,7 +10,7 @@ import org.hibernate.annotations.OnDeleteAction;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "railway.stock")
+@Table(name = "stock")
 @NoArgsConstructor
 @Data
 public class StockEntity {
