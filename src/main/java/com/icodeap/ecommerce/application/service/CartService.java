a722d80126f5c337f2d6b84ec23ec63c350744e6
jona4815 <99785626+Raque48@users.noreply.github.com>
2023-04-12 11:30:53 -0600

    Update CartService.java

--- src/main/java/com/icodeap/ecommerce/application/service/CartService.java
+++ src/main/java/com/icodeap/ecommerce/application/service/CartService.java
@@ -8,11 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
 @Service
 public class CartService {
-
+    //
     private List<ItemCart> itemCarts;
+    //paso mis 2 atributos integer objeto de itemcart para poder
+    //para que no se repita mi producto
     private HashMap<Integer, ItemCart> itemCartHashMap;
 
     public CartService() {
@@ -20,29 +21,36 @@ public class CartService {
         this.itemCarts = new ArrayList<>();
     }
 
+    //añadir productos al carro de compras 4 valores
     public void addItemCart(Integer quantity, Integer idProduct, String nameProduct, BigDecimal price){
-        ItemCart itemCart = new ItemCart(idProduct, nameProduct, quantity, price);
+    	ItemCart itemCart = new ItemCart();
+        itemCart.Inicializar(idProduct, nameProduct, quantity, price);
         itemCartHashMap.put(itemCart.getIdProduct(), itemCart);
         fillList();
     }
-
+    
+    //devuelvo el total de mi carrito de compra
     public  BigDecimal getTotalCart(){
         BigDecimal total = BigDecimal.ZERO;
+        //voy a rrecorrer mi areglo sumo y obtengo el total
         for (ItemCart itemCart : itemCarts){
             total = total.add(itemCart.getTotalPriceItem());
         }
         return total;
     }
+    
+    //ekimnar un articulo en especial
     public void removeItemCart(Integer idProduct){
         itemCartHashMap.remove(idProduct);
         fillList();
     }
-
+  //elimnar todos los articulos
     public void removeAllItemsCart(){
         itemCartHashMap.clear();;
         itemCarts.clear();
     }
 
+    //mi llista vacia 
     private void fillList(){
         itemCarts.clear();
         itemCartHashMap.forEach(
